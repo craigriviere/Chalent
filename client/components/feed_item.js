7025a8f909ed1fd88d3b701cf31c0eefacdf34a2
Craig Riviere <Craig Riviere>
2017-01-16 23:40:44 +0000

    class to className on img wrapper class

--- client/components/feed_item.js
+++ client/components/feed_item.js
@@ -4,7 +4,7 @@ import React from 'react';
 const FeedItem = () => {
   return (
     <div className="feed-item">
-      <a href="#"><div class="img-wrapper">
+      <a href="#"><div className="img-wrapper">
         <img src="http://placehold.it/600x400?text=Item" alt="Item"/>
       </div></a>
       <img src="http://placehold.it/40x40?" alt="Avatar"/>
